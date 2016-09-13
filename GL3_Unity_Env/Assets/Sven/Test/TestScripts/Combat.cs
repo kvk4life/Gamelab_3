54240e681d8521bc486620b311c098f7cad6c973
Sven <svenevertse@gmail.com>
2016-09-13 15:31:39 +0100

    Combat systeem klein beejte uitgebreid
    nu met cooldown en delegates

--- GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat.cs
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat.cs
@@ -6,20 +6,70 @@ public class Combat : MonoBehaviour {
 	public int attackDamage;
 
 	public float rayDis;
+	public float cooldownStats;
 
 	public RaycastHit rayHit;
 
-	public int [] attackDamageStats;
-	public float [] cooldownStats;
- 
+	public bool mayAttack;
+
+	public EnemyHealthTestSven healthEnemy;
+	
 	delegate void AttackDelegate(int num);
 	AttackDelegate myAttack;
 
 	void Start () {
+
+		mayAttack = true;
 	
 	}
 	
 	void Update () {
+
+		TriggerDetect ();
+		TriggerDetectTest();							//Weghalen nadat het systeem volledig werkt met controller
+	
+	}
+
+	public void TriggerDetect () {
+
+		if(Input.GetAxis("Triggers") < 0 && mayAttack == true){
+			myAttack = AttackRight;
+			myAttack(2);
+		}
+		else if(Input.GetAxis("Triggers") > 0 && mayAttack == true){
+			myAttack = AttackLeft;
+			myAttack(1);
+		}
+	}
+
+	public void AttackLeft (int num) {
+
+		print("Weapon:" + num);
+
+		StartCoroutine(CoolDown(cooldownStats));
+		if(Physics.Raycast(transform.position, transform.forward, out rayHit, rayDis)){
+			if(rayHit.transform.tag == "Enemy"){
+				healthEnemy = rayHit.transform.gameObject.GetComponent<EnemyHealthTestSven>();
+				healthEnemy.EnemyHealth(attackDamage);
+			}
+		}
+	}
+
+	public void AttackRight (int num) {
+
+		print("Weapon:" + num);
+
+	}
+
+	IEnumerator CoolDown (float cooldown){
+
+		mayAttack = false;
+		yield return new WaitForSeconds(cooldown);
+		print("Recharged");
+		mayAttack = true;
+	}
+
+	public void TriggerDetectTest (){					//Weghalen nadat het systeem volledig werkt met controller
 
 		if(Input.GetButtonDown("Attack Left")){
 			myAttack = AttackLeft;
@@ -30,18 +80,6 @@ public class Combat : MonoBehaviour {
 			myAttack = AttackLeft;
 			myAttack(2);
 		}
-	
 	}
 
-	public void AttackLeft (int num) {
-
-		print("Weapon:" + num);
-
-	}
-
-	public void AttackRight (int num) {
-
-		print("Weapon:" + num);
-
-	}
 }
---
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/EnemyHealthTestSven.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthTestSven : MonoBehaviour {
+
+	public int health;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void EnemyHealth (int damage){
+
+		health -= damage;
+
+		if(health < 1){
+			Destroy(gameObject);
+		}
+	}
+}
