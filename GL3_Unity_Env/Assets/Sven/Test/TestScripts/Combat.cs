b3e7526715920e9ab10a408b4494fea6f3c92b39
Sven <svenevertse@gmail.com>
2016-09-13 13:49:50 +0100

    Delegate GrayBox

--- GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat.cs
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat.cs
@@ -1,13 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Combat : MonoBehaviour {
+
+	public int attackDamage;
+
+	public float rayDis;
+
+	public RaycastHit rayHit;
+
+	public int [] attackDamageStats;
+	public float [] cooldownStats;
+ 
+	delegate void AttackDelegate(int num);
+	AttackDelegate myAttack;
 
 	void Start () {
 	
 	}
 	
 	void Update () {
+
+		if(Input.GetButtonDown("Attack Left")){
+			myAttack = AttackLeft;
+			myAttack(1);
+		}
+
+		if(Input.GetButtonDown("Attack Right")){
+			myAttack = AttackLeft;
+			myAttack(2);
+		}
 	
+	}
+
+	public void AttackLeft (int num) {
+
+		print("Weapon:" + num);
+
+	}
+
+	public void AttackRight (int num) {
+
+		print("Weapon:" + num);
+
 	}
 }
