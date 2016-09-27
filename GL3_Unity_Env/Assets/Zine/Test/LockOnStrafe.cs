95e1fae88082552552d9a562ea9676fd515308c8
Sven <svenevertse@gmail.com>
2016-09-27 13:32:25 +0100

    Zine zijn precum import #NammFlashbacks

---
+++ GL3_Unity_Env/Assets/Zine/Test/LockOnStrafe.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LockOnStrafe : MonoBehaviour
+{
+    public float speed;
+    public Image border1, border2;
+    public Orbit orbit;
+    public bool triggerPressed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("RB"))
+        {
+            orbit.ExitSprint();
+            orbit.camMode=CameraMode.ThirdPersonStrafe;
+            StopAllCoroutines();
+            StartCoroutine(LockOn());
+        }
+        if(Input.GetButtonUp("RB"))
+        {
+            orbit.camMode = CameraMode.ThirdPerson;
+            StopAllCoroutines();
+            StartCoroutine(LockOff());
+        }
+
+        if(Input.GetAxis("Triggers")<0)
+        {
+            orbit.ExitSprint();
+            orbit.camMode=CameraMode.FirstPerson;
+            if(triggerPressed==false)
+            {
+                StopAllCoroutines();
+                StartCoroutine(LockOn());
+                triggerPressed=true;
+            }
+
+        }
+
+        if(Input.GetAxis("Triggers")==0)
+        {
+            if(triggerPressed==true)
+            {
+                print("Lerpy");
+                orbit.camMode = CameraMode.ThirdPerson;
+                StopAllCoroutines();
+                StartCoroutine(LockOff());
+                orbit.StartCoroutine("ExitFirstPerson");
+            }
+            triggerPressed=false;
+
+        }
+    }
+
+    IEnumerator LockOn()
+    {
+        while (border1.fillAmount < 1)
+        {
+            border1.fillAmount += speed*Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+            if (border1.fillAmount > 1)
+            {
+                border1.fillAmount = 1;
+            }
+            border2.fillAmount += speed * Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+            if (border2.fillAmount > 1)
+            {
+                border2.fillAmount = 1;
+            }
+        }
+    }
+
+    IEnumerator LockOff()
+    {
+        while (border1.fillAmount > 0.5f)
+        {
+            border1.fillAmount -= speed * Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+            if (border1.fillAmount <0.5f)
+            {
+                border1.fillAmount = 0.5f;
+            }
+            border2.fillAmount -= speed * Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+            if (border2.fillAmount < 0.5f)
+            {
+                border2.fillAmount = 0.5f;
+            }
+        }
+    }
+}
---
+++ GL3_Unity_Env/Assets/Zine/Test/Orbit.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CameraMode
+{
+    ThirdPerson,
+    ThirdPersonStrafe,
+    FirstPerson,
+}
+public class Orbit : MonoBehaviour
+{
+
+    public CameraMode camMode;
+    public Animator anim;
+    public Transform lookAt;
+    public float rotSpeed;
+    public Transform camTransform;
+    public float moveSpeed;
+    public GameObject obj;
+    public Camera cam;
+    public float sensitivityX = 4.0f;
+    public float sensitivityY = 1.0f;
+    public LockOnStrafe strafe;
+    public Transform cameraLocStrafe;
+    public Transform cameraLocHead;
+    public Quaternion savedRot;
+    public bool sprint;
+    public Transform camRotator;
+    public bool lerpBack;
+    public float jumpForce;
+    // Use this for initialization
+    void Start()
+    {
+        camTransform = transform;
+
+    }
+
+    void Update()
+    {
+        switch(camMode)
+        {
+            case CameraMode.ThirdPerson:
+                obj.transform.position += obj.transform.forward * Input.GetAxis("LeftJoyY") * Time.deltaTime *  moveSpeed;
+                obj.transform.Rotate(new Vector3(0, Input.GetAxis("LeftJoyX") * rotSpeed * Time.deltaTime, 0));
+                if (transform.parent != camRotator) ;
+                {
+                    transform.SetParent(camRotator);
+                }
+                if(lerpBack==true)
+                {
+                    CameraLerp(cameraLocStrafe);
+                }
+                transform.LookAt(camRotator);
+                camRotator.Rotate(Input.GetAxis("RightJoyY") * rotSpeed * Time.deltaTime, Input.GetAxis("RightJoyX") * rotSpeed * Time.deltaTime, 0);
+                break;
+            case CameraMode.ThirdPersonStrafe:
+                CameraLerp(cameraLocStrafe);
+                transform.LookAt(camRotator);
+                obj.transform.position += obj.transform.forward * Input.GetAxis("LeftJoyY") * Time.deltaTime * moveSpeed;
+                obj.transform.position += obj.transform.right * Input.GetAxis("LeftJoyX") * Time.deltaTime * moveSpeed;
+                obj.transform.Rotate(0, Input.GetAxis("RightJoyX") * rotSpeed * Time.deltaTime, 0);
+                break;
+            case CameraMode.FirstPerson:
+                CameraLerp(cameraLocHead);
+                obj.transform.position += obj.transform.forward * Input.GetAxis("LeftJoyY") * Time.deltaTime * moveSpeed;
+                obj.transform.Rotate(new Vector3(0, Input.GetAxis("LeftJoyX") * rotSpeed * Time.deltaTime, 0));
+                transform.Rotate(-Input.GetAxis("RightJoyY") * rotSpeed * Time.deltaTime, 0, 0);
+                break;
+
+        }
+        Animate();
+        if (Input.GetButtonDown("LB") && camMode!=CameraMode.ThirdPersonStrafe)
+        {
+            EnterSprint();
+        }
+        if (Input.GetButtonUp("LB"))
+        {
+            ExitSprint();
+        }
+        if(Input.GetButtonDown("A") && camMode!=CameraMode.FirstPerson)
+        {
+            obj.GetComponent<Rigidbody>().velocity=new Vector3(0,jumpForce,0);
+        }
+
+
+    }
+
+    void EnterSprint()
+    {
+        if (sprint == false)
+        {
+            moveSpeed = moveSpeed * 4;
+            anim.speed = 4;
+            sprint = true;
+        }
+
+    }
+
+    public void ExitSprint()
+    {
+        if (sprint == true)
+        {
+            moveSpeed = moveSpeed / 4;
+            anim.speed = 1f;
+            sprint = false;
+        }
+
+    }
+
+    public IEnumerator ExitFirstPerson()
+    {
+        lerpBack=true;
+        yield return new WaitForSeconds(1f);
+        lerpBack=false;
+    }
+
+    void Animate()
+    {
+        
+    }
+
+    public void CameraLerp(Transform cameraLoc)
+    {
+        if(transform.parent!=obj.transform)
+        {
+            transform.SetParent(obj.transform);
+        }
+        transform.position = Vector3.Lerp(transform.position, cameraLoc.position, 4 * Time.deltaTime);
+    }
+}
