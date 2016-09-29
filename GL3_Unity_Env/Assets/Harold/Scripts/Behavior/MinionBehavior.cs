d19b558f4d8ae5e07d33e18cc9b9607a4f3924f3
HaroldGaming <harold-de-jong@hotmail.com>
2016-09-29 11:46:52 +0200

    Gescript
    More Behaviors

--- GL3_Unity_Env/Assets/Harold/Scripts/Behavior/MinionBehavior.cs
+++ GL3_Unity_Env/Assets/Harold/Scripts/Behavior/MinionBehavior.cs
@@ -4,127 +4,189 @@ using System.Collections.Generic;
 
 public class MinionBehavior : MonoBehaviour {
 
-    public int moveSpeed;
-    public float timeBetweenAttacks, attackRange, moveAwayTime;
-    private float distance, timer;
-    public string[] taggList;
-    public GameObject spawner;
-    private MinionSpawner spawnScript;
+    public float timeBetweenAttack, attackRange;
+    private float attackTimer;
+    public int moveSpeed, fullMoveSpeed;
+    private int enumNumber, resetMoveSpeed, counter;
     [SerializeField]
-    private Transform curretTarget;
-    private int counter, resetMoveSpeed, moveAwaySpeed;
-    public Stats statsClass;
-    private bool attackOff, moveAway;
+    private Transform currentTarget, attackTarget, wayPointTarget;
+    public GameObject spawner;
+    public string[] tagList;
+    private bool allowAttack;
+    public MinionSpawner spawnClass;
+    private Stats statsClass;
+    public States minionState;
+    public AttackTargets attackPriority;
 
-
+    [SerializeField]
     private List<Transform> wayPointList = new List<Transform>();
     [SerializeField]
     private List<GameObject> enemyList = new List<GameObject>();
     [SerializeField]
     private List<GameObject> allyList = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> minionList = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> turretList = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> championList = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> jungleList = new List<GameObject>();
+
+    public enum States {
+        Attack,
+        FollowWayPoint
+    }
+
+    public enum AttackTargets {
+        Minions,
+        Turrets,
+        Champions,
+        JungleCamp
+    }
 
 
     void Start() {
-        spawnScript = spawner.GetComponent<MinionSpawner>();
         statsClass = GetComponent<Stats>();
-        attackOff = true;
-
-        AddWayPoints();
-        curretTarget = wayPointList[counter];
+        spawnClass = spawner.GetComponent<MinionSpawner>();
         resetMoveSpeed = moveSpeed;
-        moveAwaySpeed = moveSpeed;
+        fullMoveSpeed = moveSpeed;
+        AddWayPoints();
+        wayPointTarget = wayPointList[counter];
+        allowAttack = true;
+ 
     }
 
     void Update() {
 
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        if (moveAway) {
-            if (timer >= 0) {
-                MoveAway();
-                timer -= Time.deltaTime;
-            }
-            else {
-                moveSpeed = resetMoveSpeed;
-                CheckTarget();
-                moveAway = false;   
-            }
+        if (minionState == States.FollowWayPoint) {
+            FollowWayPoint();
         }
 
-        if (statsClass.currentHealth <= 0) {
+        if (minionState == States.Attack) {
+            Attack(enumNumber = (int)attackPriority);
+        }
+
+        if (enemyList.Count == 0) {
+            int temp = (int)minionState;
+            temp = 1;
+            minionState = (States)temp;
+        }
+        else {
+            int temp = (int)minionState;
+            temp = 0;
+            minionState = (States)temp;
+        }
+        CheckPriority();
+
+        if(statsClass.currentHealth <= 0) {
             Destroy(gameObject);
         }
 
-        if (attackOff == false) {
-            if (curretTarget != null) {
-                distance = Vector3.Distance(gameObject.transform.position, curretTarget.transform.position);
-
-            }
-
-            if (distance <= attackRange) {
-                moveSpeed = 0;
-            }
-            else {
-                moveSpeed = resetMoveSpeed;
-            }
-        }
-
-        if(enemyList.Count == 0) {
-            transform.LookAt(curretTarget.position);
-        }
+        /// add function null checkers;
     }
 
-    void CheckTarget() {
 
-        if (enemyList.Count > 0) {
-            if (attackOff) {
-                if (enemyList[0] == null) {
-                    enemyList.Remove(null);
-                    CheckTarget();
-                }
-                else {
-                    curretTarget = enemyList[0].transform;
-                    attackOff = false;
-                    StartCoroutine(Attacking(timeBetweenAttacks));
-
-                }
-            }
-        }
-        else {
-            curretTarget = wayPointList[counter];
-            transform.LookAt(curretTarget.position);
-        }
-
-
-    }
-
-    IEnumerator Attacking(float attackTime) {
-
-        if (curretTarget != null) {
-            if (enemyList.Count > 0) {
-                if (distance <= attackRange) {
-                    curretTarget.GetComponent<Stats>().GetDamage(statsClass.damage);
-                    transform.LookAt(curretTarget.position);
-                }
-                else {
-                    StopCoroutine("Attacking");
-                }
-            }
-        }
-
-        if (curretTarget == null) {
-            attackOff = true;
-            moveSpeed = resetMoveSpeed;
-            CheckTarget();
-            StopCoroutine("Attacking");
-        }
-        yield return new WaitForSeconds(attackTime);
-        StartCoroutine(Attacking(timeBetweenAttacks));
+    void FollowWayPoint() {
+        transform.LookAt(wayPointTarget);
+        moveSpeed = fullMoveSpeed;
     }
 
     void AddWayPoints() {
-        for (int i = 0; i < spawnScript.waypointList.Length; i++) {
-            wayPointList.Add(spawnScript.waypointList[i]);
+        for (int i = 0; i < spawnClass.waypointList.Length; i++) {
+            wayPointList.Add(spawnClass.waypointList[i]);
+        }
+    }
+
+    void CheckPriority() {
+        if (jungleList.Count > 0) {
+            if (championList.Count > 0) {
+                if (turretList.Count > 0) {
+                    if (minionList.Count > 0) {
+                        int temp = (int)attackPriority;
+                        temp = 0;
+                        attackPriority = (AttackTargets)temp;
+                    }
+                    else {
+                        int temp = (int)attackPriority;
+                        temp = 1;
+                        attackPriority = (AttackTargets)temp;
+                    }
+                }
+                else {
+                    int temp = (int)attackPriority;
+                    temp = 2;
+                    attackPriority = (AttackTargets)temp;
+                }
+            }
+            else {
+                int temp = (int)attackPriority;
+                temp = 3;
+                attackPriority = (AttackTargets)temp;
+            }
+        }
+    }
+
+    void Attack(int number) {
+     
+        if (attackTarget != null) {
+            transform.LookAt(attackTarget);
+
+            float distance = Vector3.Distance(gameObject.transform.position, attackTarget.transform.position);
+
+            if (distance <= attackRange) {
+                allowAttack = true;
+                moveSpeed = 0;
+            }
+            else {
+                allowAttack = false;
+                moveSpeed = fullMoveSpeed;
+            }
+        }
+        else {
+            switch (number) {
+                case 0:
+                    minionList.Remove(null);
+                    break;
+                case 1:
+                    turretList.Remove(null);
+                    break;
+                case 2:
+                    championList.Remove(null);
+                    break;
+                case 3:
+                   jungleList.Remove(null);
+                    break;
+            }
+        }
+
+        if (allowAttack) { 
+            if (attackTimer <= 0) {
+                attackTimer = timeBetweenAttack;
+                switch (number) {
+                    case 0:
+                        attackTarget = minionList[0].transform;
+                        minionList[0].GetComponent<Stats>().currentHealth -= statsClass.damage;
+                        break;
+                    case 1:
+                        attackTarget = turretList[0].transform;
+                        turretList[0].GetComponent<Stats>().currentHealth -= statsClass.damage;
+                        break;
+                    case 2:
+                        attackTarget = championList[0].transform;
+                        championList[0].GetComponent<Stats>().currentHealth -= statsClass.damage;
+                        break;
+                    case 3:
+                        attackTarget = jungleList[0].transform;
+                        jungleList[0].GetComponent<Stats>().currentHealth -= statsClass.damage;
+                        break;
+                }
+            }
+            else {
+                attackTimer -= Time.deltaTime;
+            }
         }
     }
 
@@ -134,12 +196,13 @@ public class MinionBehavior : MonoBehaviour {
             if (counter >= wayPointList.Count) {
                 counter = 0;
             }
+            wayPointTarget = wayPointList[counter];
         }
 
         if (trigger.gameObject.GetComponent<Stats>() != null) {
             AgroAdd(trigger.gameObject);
         }
-        CheckTarget();
+
     }
 
     void OnTriggerExit(Collider trigger) {
@@ -150,8 +213,8 @@ public class MinionBehavior : MonoBehaviour {
     }
 
     void AgroRemove(GameObject target) {
-        for (int i = 0; i <= taggList.Length - 1; i++) {
-            if (target.transform.tag == taggList[i]) {
+        for (int i = 0; i <= tagList.Length - 1; i++) {
+            if (target.transform.tag == tagList[i]) {
                 if (target.gameObject.GetComponent<Stats>().teamNumber != statsClass.teamNumber) {
                     enemyList.Remove(target);
                 }
@@ -164,13 +227,14 @@ public class MinionBehavior : MonoBehaviour {
     }
 
     void AgroAdd(GameObject target) {
-        for (int i = 0; i <= taggList.Length - 1; i++) {
-            if (target.transform.tag == taggList[i]) {
+        for (int i = 0; i <= tagList.Length - 1; i++) {
+            if (target.transform.tag == tagList[i]) {
                 if (target.gameObject.GetComponent<Stats>().teamNumber != statsClass.teamNumber) {
                     if (enemyList.Contains(target)) {
                     }
                     else {
                         enemyList.Add(target);
+                        AddToEntityList(target);
                     }
                     break;
                 }
@@ -182,22 +246,22 @@ public class MinionBehavior : MonoBehaviour {
                     }
                     break;
                 }
-                //curretTarget = target.transform;
-
             }
         }
     }
 
-    public void OnCollisionEnter(Collision other) {
-        if (allyList.Contains(other.gameObject)) {
-            timer = moveAwayTime;
-            moveAway = true;
+    void AddToEntityList(GameObject entity) {
+        if (entity.transform.tag == tagList[0]) {
+            minionList.Add(entity);
         }
-    }
-
-    void MoveAway() {
-        StopCoroutine("Attacking");
-        transform.Translate(Vector3.left * moveAwaySpeed * Time.deltaTime);
-
+        if (entity.transform.tag == tagList[1]) {
+            turretList.Add(entity);
+        }
+        if (entity.transform.tag == tagList[2]) {
+            championList.Add(entity);
+        }
+        if (entity.transform.tag == tagList[3]) {
+            jungleList.Add(entity);
+        }
     }
 }
