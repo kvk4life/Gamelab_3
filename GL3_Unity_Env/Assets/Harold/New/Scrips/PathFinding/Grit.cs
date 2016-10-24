a7b9addb8022cc5cb5a6bfc239d301048883afcb
HaroldGaming <harold-de-jong@hotmail.com>
2016-10-24 11:33:51 +0200

    Scripting
    .

---
+++ GL3_Unity_Env/Assets/Harold/New/Scrips/PathFinding/Grit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grit : MonoBehaviour {
+
+    public LayerMask unwalkebleMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start() {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid() {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for(int x = 0; x < gridSizeX; x++) {
+            for(int y = 0; y < gridSizeY; x++) {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkeble = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkebleMask));
+                grid[x, y] = new Node(walkeble, worldPoint);
+            }
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if (grid != null)
+            foreach(Node n in grid) {
+                Gizmos.color = (n.walkeble) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+    }
+
+}
---
+++ GL3_Unity_Env/Assets/Harold/New/Scrips/PathFinding/Node.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+
+    public bool walkeble;
+    public Vector3 worldPosition;
+
+    public Node(bool _walkeble, Vector3 _worldPos) {
+        walkeble = _walkeble;
+        worldPosition = _worldPos;
+    }
+
+}
