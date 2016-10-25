aafeb53258984666135c630668847afef89b4262
HaroldGaming <harold-de-jong@hotmail.com>
2016-10-25 16:02:34 +0200

    Scripts

--- GL3_Unity_Env/Assets/Harold/New/Scrips/PathFinding/Grit.cs
+++ GL3_Unity_Env/Assets/Harold/New/Scrips/PathFinding/Grit.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Grit : MonoBehaviour {
 
+    public bool onlyDisplayPathGizmos;
     public LayerMask unwalkebleMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -18,27 +20,80 @@ public class Grit : MonoBehaviour {
         CreateGrid();
     }
 
+    public int MaxSize {
+        get {
+            return gridSizeX * gridSizeY;
+
+        }
+    }
+
     void CreateGrid() {
         grid = new Node[gridSizeX, gridSizeY];
         Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
 
         for(int x = 0; x < gridSizeX; x++) {
-            for(int y = 0; y < gridSizeY; x++) {
+            for(int y = 0; y < gridSizeY; y++) {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkeble = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkebleMask));
-                grid[x, y] = new Node(walkeble, worldPoint);
+                grid[x, y] = new Node(walkeble, worldPoint, x, y);
             }
         }
     }
 
+    public List<Node> GetNeighbours(Node node) {
+        List<Node> neightbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if(x == 0 && y == 0)
+                    continue;
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if(checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+                    neightbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+        return neightbours;
+    }
+
+    public Node NodeFromWorldPoint(Vector3 wordlPosition) {
+        float percentX = ( wordlPosition.x + gridWorldSize.x /2) / gridWorldSize.x;
+        float percentY = (wordlPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
+
+    public List<Node> path;
     void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if (grid != null)
-            foreach(Node n in grid) {
-                Gizmos.color = (n.walkeble) ? Color.white : Color.red;
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+        if (onlyDisplayPathGizmos) {
+            if (path != null) {
+                foreach (Node n in path) {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
             }
-    }
+        }
+        else {
+            if (grid != null) {
+                foreach (Node n in grid) {
+                    Gizmos.color = (n.walkeble) ? Color.white : Color.red;
 
+                    if (path != null) {
+                        if (path.Contains(n))
+                            Gizmos.color = Color.black;
+                    }
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
+            }
+        }
+    }
 }
