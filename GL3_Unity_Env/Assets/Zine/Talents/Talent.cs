ddf6fc75cfcccf2dabc2d6c9bb6321aa7d2e9037
Zinedine038 <zsghari@hotmail.com>
2016-11-02 09:43:04 +0100

    cancer
    moar cancer

---
+++ GL3_Unity_Env/Assets/Zine/Talents/Talent.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public enum TalentEffect
+{
+    StatIncrease,
+    AbilityGet,
+    TotalDamageIncrease,
+    Buff,
+}
+
+public enum StatToIncrease
+{
+
+}
+
+
+public class Talent : MonoBehaviour
+{
+    public TalentTreeManager manager;
+    public TalentTypes thisType;
+    public TalentEffect talentEffect;
+    public UnityEvent methods;
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            methods.Invoke();
+        }
+    }
+
+
+    public void YarakTalent()
+    {
+        print("Wajow");
+    }
+
+    public void YarakTalent2()
+    {
+        print("2");
+    }
+}
