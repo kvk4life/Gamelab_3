d450e8281a3dbfbd1a32001a102be0dcbe287879
Zinedine038 <zsghari@hotmail.com>
2016-11-10 11:49:15 +0100

    spell precum

---
+++ GL3_Unity_Env/Assets/Zine/Test/Spellcast.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Spellcast : MonoBehaviour
+{
+    [SerializeField]
+    public UnityEvent spellToCast;
+    public Image bar;
+    public float castTime;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Cast");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Cast()
+    {
+        float curCast = 0;
+        while (curCast < castTime)
+        {
+            bar.fillAmount = (curCast / castTime);
+            curCast += 2 * Time.deltaTime;
+            print(curCast);
+            yield return null;
+        }
+    }
+}
---
+++ GL3_Unity_Env/Assets/Zine/Test/Spells/BaseSpel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseSpel : MonoBehaviour
+{
+    public float damage;
+    public float projectileSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * projectileSpeed * Time.deltaTime;
+    }
+}
