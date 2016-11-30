eee337e459b86034e58be992c37da22089694598
Carlo Akuma <carloakuma@gmail.com>
2016-11-30 10:48:49 +0100

    Cancer COmmit Mappen
    AY

---
+++ GL3_Unity_Env/Assets/_Scripts/Test/cancer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cancer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")));
+	}
+}
