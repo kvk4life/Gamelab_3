1c565e84475fd421e46a5d753a41c5faf853cb61
Sven <svenevertse@gmail.com>
2017-01-10 14:02:31 +0100

    ca

---
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat/Crosbow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosbow : Weapon_Manager {
+
+	void Start () {
+
+		base.mayFire = true;
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+	protected override void FireWeapon (){
+
+
+
+	}
+
+	protected override void FillDelegate (){
+
+
+	}
+		
+	protected override void WeaponStats (int damage, float rPM, float range, float speed){
+
+
+
+	}
+
+	protected override IEnumerator Cooldown (float cooldown){
+
+		base.mayFire = false;
+		yield return new WaitForSeconds(cooldown);
+		base.mayFire = true;
+
+	}
+}
---
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat/Weapon_Manager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+abstract public class Weapon_Manager : MonoBehaviour {
+
+	public bool mayFire;
+
+	protected abstract void FireWeapon ();
+
+	protected abstract void FillDelegate ();
+		
+	protected abstract void WeaponStats (int damage, float rPM, float range, float speed);
+
+	protected abstract IEnumerator Cooldown (float cooldown);
+
+}
