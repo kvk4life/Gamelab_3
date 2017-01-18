62086a710c707aec04caeeb7ef7b0d0125147517
M. Mendes Moreira <kcamvankester@hotmail.com>
2017-01-19 00:08:15 +0100

    straatlampen geadd

---
+++ GL3_Unity_Env/Assets/DemonComplete/DemonRag.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonRag : MonoBehaviour {
+
+	public Rigidbody[] bones;
+	public Animator anim;
+
+	public bool assRape = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+
+		bones = GetComponentsInChildren<Rigidbody> ();
+
+		foreach (Rigidbody rb in bones) 
+		{
+			rb.isKinematic = true;
+		}
+	
+	}
+
+	void RagActive()
+	{
+		anim.enabled = false;
+		foreach (Rigidbody rb in bones) 
+		{
+			rb.isKinematic = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (assRape) 
+		{
+			RagActive ();
+		}
+	}
+}
