6ed65898e3608bea4f5121c0a251233df3c85848
Sven <svenevertse@gmail.com>
2017-01-24 16:03:33 +0100

    Health player

---
+++ GL3_Unity_Env/Assets/Sven/Test/TestScripts/Combat/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int maxHealth;
+	public int health;
+	public Animator anim;
+
+	void Start () {
+
+		health = maxHealth;
+	
+	}
+
+	void Update () {
+
+
+	
+	}
+
+	public void GetDamage (int damage) {
+
+		health -= damage;
+		anim.SetTrigger("Get Hit");
+
+		if(health <= 0){
+
+			Destroy(gameObject);
+
+		}
+	}
+}
